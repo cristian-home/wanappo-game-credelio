a84ffca8258dbcac98124e79b1cb44778afcc84d
Cristian Home <cristian_david033@hotmail.com>
2025-06-11 23:22:10 -0500

    feat: :sparkles: Remove About link from navigation and add Game Won view

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@ import { RouterLink, RouterView } from 'vue-router'
     <div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/GameWonView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Game Won View</h1>
+    <p>Congratulations! You have won the game!</p>
+  </main>
+</template>
+
+<style scoped lang="css"></style>
